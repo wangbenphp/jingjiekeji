2d2bcde086cce30bfd0e6b29a8857ed1160b03af
wangbenphp <wangben1203@qq.com>
2018-08-06 13:03:56 +0800

    modify

--- app/Http/Controllers/Location/DataskyController.php
+++ app/Http/Controllers/Location/DataskyController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Location;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use WbPHPLibraryPackage\Service\Log;
+use WbPHPLibraryPackage\Service\Redis;
 
 /**
  * DataSky WIFI定位系统控制器
@@ -14,7 +14,8 @@ class DataskyController extends Controller
     public function index(Request $request)
     {
         $info = $request->input('data');
-        Log::info($info);
+        Redis::getInstance()->setex(time() . mt_rand(100, 999), 1000, $info);
+        exit;
         if ($info) {
             $res = logic('Datasky')->data_dispose($info);
             if ($res) {
